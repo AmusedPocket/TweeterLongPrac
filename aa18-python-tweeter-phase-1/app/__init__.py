a725293a8cd1726d18fbff3c0e6fe907f4f574f1
myunghun choi <myunghun0721@gmail.com>
2024-02-06 14:14:33 -0800

    phase 4 done

--- aa18-python-tweeter-phase-1/app/__init__.py
+++ aa18-python-tweeter-phase-1/app/__init__.py
@@ -1,9 +1,12 @@
 # !!START
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from .config import Config
 from .tweets import tweets
-from random import choice
+from random import choice, randint
 from .form.form import Tweet
+from datetime import datetime, timedelta
+
+
 
 app = Flask(__name__)
 
@@ -17,10 +20,26 @@ def index():
 
 @app.route("/feed")
 def feed():
-    return render_template("feed.html", tweets=tweets)
+    tweets_sorted = sorted(tweets, key=lambda i: datetime.strptime(i['date'], '%m/%d/%y'), reverse=True)
+    # print(tweets_sorted)
+    return render_template("feed.html", tweets=tweets_sorted)
 
-@app.route('/new')
+@app.route('/new', methods=['GET', 'POST'])
 def form():
     form = Tweet()
-    return render_template("new_tweet.html", form=form)
+    if form.validate_on_submit():
+        params = {
+            "id": len(tweets) + 1,
+            "author": form.data["author"],
+            "date": datetime.now().strftime("%m/%d/%y"),
+            "tweet" : form.data["tweet"],
+            "likes" : randint(10, 100000)
+        }
+        tweets.append(params)
+        print(tweets)
+        return redirect('/feed')
 
+    if form.errors:
+        return redirect('/')
+
+    return render_template("new_tweet.html", form=form)
--- aa18-python-tweeter-phase-1/app/form/form.py
+++ aa18-python-tweeter-phase-1/app/form/form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
 
 class Tweet(FlaskForm):
-    author = StringField("Author")
-    tweet = StringField("Tweet")
+    author = StringField("Author", validators=[InputRequired()])
+    tweet = StringField("Tweet", validators=[InputRequired()])
     submitButton = SubmitField("Create Tweet")
