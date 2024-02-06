33375a1668035ccc28729e117574ee739adb0eb5
myunghun choi <myunghun0721@gmail.com>
2024-02-06 13:04:30 -0800

    phase two done

--- aa18-python-tweeter-phase-1/app/__init__.py
+++ aa18-python-tweeter-phase-1/app/__init__.py
@@ -1,8 +1,19 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
+from .tweets import tweets
+from random import choice
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+
+@app.route("/")
+def index():
+    tweet = choice(tweets)
+    return render_template("index.html", tweet=tweet)
+
+@app.route("/feed")
+def feed():
+    return render_template("feed.html", tweets=tweets)
